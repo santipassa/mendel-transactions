68ff880cd2ffb94adff0ef5ef5913e7a0abc3c2f
Santiago Passalacqua <passalacquasantiago@gmail.com>
2022-09-17 14:58:41 -0300

    Isolate tests

--- src/test/java/com/mendel/transactions/integration/TransactionsControllerTests.java
+++ src/test/java/com/mendel/transactions/integration/TransactionsControllerTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class TransactionsControllerTests {
 
     @Autowired
